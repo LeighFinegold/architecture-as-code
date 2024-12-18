bc3916c5133a2ee799521b80964244ba67fe92db
LeighF <leigh_finegold@hotmail.co.uk>
2024-12-18 13:39:04 +0000

    Add support for offline mode where urls are mapped to local file location

--- docify/src/generators/sad-template-generator.ts
+++ docify/src/generators/sad-template-generator.ts
@@ -2,13 +2,31 @@ import {Architecture} from "../models/architecture";
 import path from "path";
 import * as fs from "fs";
 import Handlebars from 'handlebars';
+import {Relationship} from "../models/relationship";
 
 export function generateMarkdownForArchitecture(architecture: Architecture): string {
     const templatePath = path.join(__dirname, 'templates', 'sad-template.hbs');
     const template = fs.readFileSync(templatePath, 'utf-8');
-    Handlebars.registerHelper('getProperty', function(obj, property) {
+    Handlebars.registerHelper('getProperty', function (obj, property) {
         return obj[property];
     });
+
+    Handlebars.registerHelper('getInteractRelationships', function (relationships: Relationship[]) {
+        return relationships.filter(r => r["relationship-type"]["interacts"]);
+    });
+
+    Handlebars.registerHelper('getConnectRelationships', function (relationships: Relationship[]) {
+        return relationships.filter(r => r["relationship-type"]["connects"]);
+    });
+
+    Handlebars.registerHelper('getDeployedInRelationship', function (relationships: Relationship[]) {
+        return relationships.filter(r => r["relationship-type"]["connects"]);
+    });
+
+    Handlebars.registerHelper('getContainsRelationships', function (relationships: Relationship[]) {
+        return relationships.filter(r => r["relationship-type"]["connects"]);
+    });
+
     const compiledTemplate = Handlebars.compile(template);
 
     return compiledTemplate(architecture);
--- docify/src/index.ts
+++ docify/src/index.ts
@@ -1,3 +1,7 @@
-export function greet(name: string): string {
-    return `Hello, ${name}!`;
-}
+import {Docify, DocifyMode, OutputFormat} from "./docify";
+import * as fs from "fs";
+
+const docify = new Docify();
+
+docify.execute("/Users/Leigh/IdeaProjects/architecture-as-code/calm/samples/2024-12/traderx/traderx.json", DocifyMode.OFFLINE, OutputFormat.SAD_TEMPLATE)
+    .then(mdOutput => fs.writeFileSync("./sad-template.md", mdOutput) )
